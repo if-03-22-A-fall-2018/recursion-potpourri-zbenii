5db074c15b30fbef046ace10c3e443a281c4d6d3
zbenii <besic.beni@gmx.at>
2019-02-26 11:31:51 +0100

    some changes

--- recfuncs.cpp
+++ recfuncs.cpp
@@ -13,11 +13,21 @@ int convert_to_binary(int n){
 }
 
 int* create_array(int size){
-  return 0;
+  int* array=new int[size];
+
+  for (int i = 0; i < size; i++){
+    array[i]=-1;
+  }
+
+  return array;
 }
 
 int calc_factorial(int n){
-  return 0;
+  if(n==0)
+  {
+    return 1;
+  }
+  return n*calc_factorial(n-1);
 }
 
 int calc_sums(int* n, int count, int* out_arr, int* out_arr_bin){
